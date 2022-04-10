fcb618bb096df8802fcf527d3058259b566111ad
FabioDiasRC <fabior.carvalho@hotmail.com>
2022-04-10 14:15:31 -0300

    finalização do curso introdutorio html e css

---
+++ modulo2/desenvolvimentoWeb/projeto.php
@@ -0,0 +1,16 @@
+<html>
+
+  <head>
+    <title>site php_check_syntax</title>
+  </head>
+
+  <body>
+
+    <?php
+      for ( $1 = 0; $i < 10; $i++) {
+        print ("linha numero" . $1 . "<br/>");
+      }
+    ?>
+    
+  </body>
+</html>
